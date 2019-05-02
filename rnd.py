eb5d530fda04a65d2a434168ba31ed291f9cf0ca
Benjamin <benjamin.bonini@gmx.ch>
2019-05-01 19:53:40 -0700

    added rnd file

---
+++ rnd.py
@@ -0,0 +1,9 @@
+def workerStrike():
+    print("Workers are striking and half your resources have been stealing")
+def factoryFire():
+    print("Your factory is on fire")
+def worldWar():
+    print("The world falls into total war")
+def theGreatDepression():
+    print("The great Depression")
+def 
