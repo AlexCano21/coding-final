04e7fe3c162726f786cb44cc69854b9ee1341dad
Cano <169815@ouhsd.us>
2019-05-08 11:18:32 -0700

    New

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ from tkinter import Button
 
 
 import time
-
-
+import rnd as rnd
+import random
 from multiprocessing import Process
 
 #Import time 
@@ -52,7 +52,7 @@ call('cls', shell=True)
 
 #Balancing/Global Variables
 #current stage
-stage = int(data[0].strip("\n"), 10)
+# stage = int(data[0].strip("\n"), 10)
 #counter variable
 x = 0
 #how long inbetween spawning cycles in seconds
@@ -79,6 +79,8 @@ copperSmeltRate = round(diff)
 ironSmeltRate = round(diff*2)
 #Coal usage for normal furnace in 1 cycle
 coalSmeltRate = round(diff*4)
+#how big the chance is that a random event occurs in a 10 cycles 1:chance --> 1:1000 
+chance = 1
 
 ##################################################################
 
@@ -136,7 +138,6 @@ def updateIron(minplu, amount):
     data[0] = str(val) + "\n"
     with open('items.txt', 'w') as file:
         file.writelines( data ) 
-
 def updateCoal(minplu, amount):
     with open('items.txt', 'r') as file:
         # read a list of lines into data
@@ -834,6 +835,24 @@ titleLabel.after(titleScreenTime, switchToGame)
 
 ##################################################################
 
+#picking a random event
+def randomEvent():
+    rndNumber = random.randint(0, 6)
+    if rndNumber == 1:
+        rnd.workerStrike()
+    elif rndNumber == 2:
+        rnd.factoryFire()
+    elif rndNumber == 3:
+        rnd.worldWar()
+    elif rndNumber == 4:
+        rnd.theGreatDepression()
+    elif rndNumber == 5:
+        rnd.theSnap()
+    elif rndNumber == 6:
+        rnd.elonInvests()
+
+
+
 #mainloop running
 def loop():
     root.mainloop()
@@ -855,6 +874,11 @@ def spawnerTimer():
         updateCopper(2, copperSpawnRate*copperSpawner)
         if x%4 == 0:
             furnaceTimer()
+        elif x%10 == 0:
+            print(chance)
+            rndnr = random.randint(0, chance)
+            if rndnr <= 1:
+                randomEvent()
         if p1.is_alive() == False:
             break
 
